aa302e02310adf71c09514b9d875bab87312981d
Kevin Wang <b09855121@gmail.com>
2017-01-07 16:46:01 +0800

    Add files via upload

---
+++ js/temp.js
@@ -0,0 +1,36 @@
+
+ //hamburger slide
+ $(function(){
+   $(".site-ham #hamburger").click(function(){
+     var $ul = $(this).parent("nav").children("ul");
+     if($ul.hasClass("hamburger-click")){
+       $ul.removeClass("hamburger-click");
+       $ul.slideUp();
+     }
+     else{
+       $ul.addClass("hamburger-click");
+       $ul.slideDown();
+       return false;
+     }
+   });
+ });
+ $(document).ready(function(){
+	$('#hamburger').click(function(){
+		$(this).toggleClass('open');		
+	});
+});
+
+ $(function(){
+ $('#next').click(function(){
+    $('html,body').animate({
+      scrollTop: $('#cont').offset().top
+    }, 1000);
+    return false;
+  });
+  });
+  $(function(){//go to the top
+  $("#top").click(function(){
+    $("html,body").animate({scrollTop:0},800);
+      return false;
+  });
+});

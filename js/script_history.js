66af5bedbdfc54b7530baa45098aaf19334a6c5c
CherylHuang <jjccbb112233@gmail.com>
2017-01-09 02:52:45 +0800

    Add files via upload

--- js/script_history.js
+++ js/script_history.js
@@ -8,3 +8,23 @@ $(function(){
       $("#time_line").css("height", "0"+h);
    });
 });
+
+// map2 change
+$(function(){
+  $("#map2_btn").click(function() {
+    if($(this).hasClass("click")){
+      $(this).removeClass("click");
+      $("#map img").attr("src","img/history/map.png");
+    }
+    else{
+      $(this).addClass("click");
+      $("#map img").attr("src","img/history/map2.png");
+      return false;
+    }
+  });
+});
+
+//heart move on timeline
+$(function(){
+  
+});

208df3582bbee8f066746fd68bea979db8e6ed3f
CherylHuang <jjccbb112233@gmail.com>
2017-01-10 22:59:11 +0800

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,16 @@
 $(function(){
-  var config = {
-    apiKey: "AIzaSyD4I6Hcv-lh_JfLWvhpGegD9ZXQQvF-m7k",
-    authDomain: "final-24940.firebaseapp.com",
-    databaseURL: "https://final-24940.firebaseio.com",
-    storageBucket: "final-24940.appspot.com",
-    messagingSenderId: "508634277677"
-  };
-  firebase.initializeApp(config);
+  // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyD4I6Hcv-lh_JfLWvhpGegD9ZXQQvF-m7k",
+      authDomain: "final-24940.firebaseapp.com",
+      databaseURL: "https://final-24940.firebaseio.com",
+      storageBucket: "final-24940.appspot.com",
+      messagingSenderId: "508634277677"
+    };
+    firebase.initializeApp(config);
 
   // Firebase database reference
-  var dbChatRoom = firebase.database().ref().child('chatroom');
+  var dbLogIn = firebase.database().ref().child('login');
   var dbUser = firebase.database().ref().child('user');
   var dbRef = firebase.database().ref();
 
@@ -18,101 +19,14 @@ $(function(){
 
 
   // REGISTER DOM ELEMENTS
-  var $messageField = $('#messageInput');
-  var $messageList = $('#example-messages2');
-
-  // REGISTER DOM ELEMENTS
   const $email = $('#email');
   const $password = $('#password');
   const $btnSignIn = $('#btnSignIn');
   const $btnSignUp = $('#btnSignUp');
   const $btnSignOut = $('#btnSignOut');
-  const $hovershadow = $('.hover-shadow');
   const $btnSubmit = $('#btnSubmit');
   const $signInfo = $('#sign-info');
 
-  //profile elements
-  const $file = $('#file');
-  const $profileName = $('#profile-name');
-  const $profileEmail = $('#profile-email');
-  const $profileOccupation = $('#profile-occupation');
-  const $profileAge = $('#profile-age');
-  const $profileDescriptions = $('#profile-descriptions');
-
-  //input ELEMENTS
-  const $InputOccupation = $('#input_occu');
-  const $InputAge = $('#input_age');
-  const $InputDescriptions = $('#input_des');
-
-
-//chat room
-
-firebase.auth().onAuthStateChanged(function(user){
-    if(user){
-    var use = firebase.auth().currentUser;
-    const dbUserid = dbUser.child(use.uid);
-
-$messageField.keypress(function (e) {
-  if (e.keyCode == 13) {
-
-    var use = firebase.auth().currentUser;
-
-    //FIELD VALUES
-    var username = use.displayName;
-    var message = $messageField.val();
-    var picture = use.photoURL;
-    console.log(username);
-    console.log(message);
-
-    //SAVE DATA TO FIREBASE AND EMPTY FIELD
-    dbRef.push({name:username, text:message, pic:picture});
-    $messageField.val('');
-  }
-});
-
-// Add a callback that is triggered for each chat message.
-dbRef.limitToLast(10).on('child_added', function (snapshot) {
-
-  var use = firebase.auth().currentUser;
-
-  //GET DATA
-  var data = snapshot.val();
-  var username = data.name || "anonymous";
-  var message = data.text || "";
-  var pic = data.pic;
-
-  //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-  var $messageElement = $("<li>");
-  var $nameElement = $("<strong class='example-chat-username'></strong>");
-  console.log("888");
-
-  if(message != ""){
-    $nameElement.text(username).prepend($('<img>',{class:'chatimg',src:data.pic}));
-    $messageElement.text(message).prepend($nameElement);
-
-    //ADD MESSAGE
-    $messageList.append($messageElement);
-  }
-
-  //SCROLL TO BOTTOM OF MESSAGE LIST
-  $messageList[0].scrollTop = $messageList[0].scrollHeight;
-});
-
-}
-});
-
-//chat room end
-
-
-  // Hovershadow
-  $hovershadow.hover(
-    function(){
-      $(this).addClass("mdl-shadow--4dp");
-    },
-    function(){
-      $(this).removeClass("mdl-shadow--4dp");
-    }
-  );
 
   var storageRef = firebase.storage().ref();
 
@@ -200,36 +114,20 @@ dbRef.limitToLast(10).on('child_added', function (snapshot) {
       console.log(user);
       const loginName = user.displayName || user.email;
       const dbUserid = dbUser.child(use.uid);
-      var $occu = dbUserid.child('Occupation');
-      var $age = dbUserid.child('Age');
-      var $des = dbUserid.child('Descriptions');
       $signInfo.html(loginName+" is login...");
       $btnSignIn.attr('disabled', 'disabled');
       $btnSignUp.attr('disabled', 'disabled');
       $btnSignOut.removeAttr('disabled')
       $profileName.html(user.displayName);
       $profileEmail.html(user.email);
-      $img.attr("src",user.photoURL);
 
-      // show on profile
-      $occu.on('value', function(snap){
-        $profileOccupation.html(snap.val());
-      });
-      $age.on('value', function(snap){
-        $profileAge.html(snap.val());
-      });
-      $des.on('value', function(snap){
-        $profileDescriptions.html(snap.val());
-      });
+      $(document).delay(2000).close();
 
     } else{
     console.log("not logged in");
       $profileName.html("N/A");
       $profileEmail.html('N/A');
-      $img.attr("src","");
-      $profileOccupation.html("N/A");
-      $profileAge.html("N/A");
-      $profileDescriptions.html("N/A");
+
     }
   });
 
@@ -245,52 +143,5 @@ dbRef.limitToLast(10).on('child_added', function (snapshot) {
     location.reload();
   });
 
-  // Submit
-  $btnSubmit.click(function(){
-    var user = firebase.auth().currentUser;
-    const $userName = $('#userName').val();
-
-
-    //get input value
-    const dbUserid = dbUser.child(user.uid);
-    var occu = $InputOccupation.val();
-    var age = $InputAge.val();
-    var des = $InputDescriptions.val();
-
-    dbUserid.set({Age:age, Descriptions:des, Occupation:occu});
-
-    var $occu = dbUserid.child('Occupation');
-    var $age = dbUserid.child('Age');
-    var $des = dbUserid.child('Descriptions');
-    // show on profile
-      $occu.on('value', function(snap){
-        $profileOccupation.html(snap.val());
-      });
-      $age.on('value', function(snap){
-        $profileAge.html(snap.val());
-      });
-      $des.on('value', function(snap){
-        $profileDescriptions.html(snap.val());
-      });
-
-
-    const promise = user.updateProfile({
-      displayName: $userName,
-      photoURL: photoURL
-    });
-    promise.then(function() {
-      console.log("Update successful.");
-      user = firebase.auth().currentUser;
-
-      if (user) {
-        $profileName.html(user.displayName);
-        $profileEmail.html(user.email);
-        $img.attr("src",user.photoURL);
-
-        const loginName = user.displayName || user.email;
-        $signInfo.html(loginName+" is login...");
-      }
-    });
-  });
 
 });

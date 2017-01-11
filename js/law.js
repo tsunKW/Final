3119848a532be4d256ef003fd30e89f6c1366527
CherylHuang <jjccbb112233@gmail.com>
2017-01-12 04:22:56 +0800

    Add files via upload

---
+++ js/law.js
@@ -0,0 +1,17 @@
+$(function(){
+  var width = $(window).width();
+
+  if(width > 780){
+    $("#st1").click(function(){$("#exp1").fadeIn();});
+    $("#st2").click(function(){$("#exp2").fadeIn();});
+    $("#st3").click(function(){$("#exp3").fadeIn();});
+    $("#st4").click(function(){$("#exp4").fadeIn();});
+    $("#st5").click(function(){$("#exp5").fadeIn();});
+    $("#st6").click(function(){$("#exp6").fadeIn();});
+
+    $(".explane img").click(function(){
+      $(this).parent(".explane").fadeOut();
+    });
+  }
+
+});
